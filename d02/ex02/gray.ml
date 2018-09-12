411f8917ecdd32d0f77cc29cd8aca4019e056010
vincent <vinc@42.fr>
2018-09-12 18:18:15 +0200

    gray work but bad formatting

--- d02/ex02/gray.ml
+++ d02/ex02/gray.ml
@@ -1,26 +1,23 @@
-let int_to_binaryString n =
+let int_to_binary n =
   let rec loop x acc = match x with
     | x when x > 0 -> loop (x / 2) ((string_of_int (x mod 2)) ^ acc)
     | _ -> acc
   in loop n ""
 
-let pad_binaryString string max =
-  let rec loop str = match str with
-    | str when (String.length str) < max -> loop ("0" ^ str)
-    | _ -> str
-  in loop string
+let nextState i = i lxor (i lsr 1)
 
-let print_binaryString i max =
-  let str = pad_binaryString (int_to_binaryString i) max
-  in print_endline str
-
-let next i = i lxor (i lsr 1)
+let pad str n =
+  let rec loop acc = match acc with
+    | str when (String.length str) < n -> loop ("0" ^ acc)
+    | _ -> acc
+  in loop str
 
 let gray n =
-  let max = String.length (int_to_binaryString n) in
-  let rec loop i = match i with
-    | i when i <= max -> print_binaryString (next i) max ; loop (i + 1)
-    | _ -> ()
+  let rec loop i =
+    let str = int_to_binary (nextState i) in
+    let length = String.length str in match str with
+      | str when length <= n -> print_endline (pad str n) ; loop (i + 1)
+      | _ -> ()
   in loop 0
 
 let () = gray 3
